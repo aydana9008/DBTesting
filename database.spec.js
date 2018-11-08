8566531850ea25ef39ad86c00827ad311392041b
agaly <a.jumaklychev@gmail.com>
2018-11-07 16:07:30 -0800

    all

--- database.spec.js
+++ database.spec.js
@@ -1,14 +1,5 @@
 describe('searching movie titles on amazon', () => {
-<<<<<<< HEAD
 // Hello I made change yavuz
-||||||| merged common ancestors
-// Hello I made change mehmet
-=======
-//Hello This is Mehmet again :)  lkjkljffs
-    
-    
-    
->>>>>>> 1c9d89921e12931ca8b36775be6ea5591e43c23f
     var pgp = require('pg-promise')(/*options*/);
     var cn = {
     host: 'localhost',
@@ -23,22 +14,10 @@ describe('searching movie titles on amazon', () => {
     
     
    
-<<<<<<< HEAD
     it('should pass movie titles on amazon search box starting with "L"', () => {
-||||||| merged common ancestors
-    it('should pass movie titles on amazon search box starting with "Z"', () => {
-=======
-    it('should pass movie titles on amazon search box starting with "H"', () => {
->>>>>>> yavuzBranch
         browser.ignoreSynchronization = true;
         browser.get("https://www.amazon.com");
-<<<<<<< HEAD
         db.any(`select title from film where title LIKE 'L%'`).then(result=>{
-||||||| merged common ancestors
-        db.any(`select title from film where title LIKE 'Z%'`).then(result=>{
-=======
-        db.any(`select title from film where title LIKE 'H%'`).then(result=>{
->>>>>>> yavuzBranch
           arr = result;
             }).catch(error=>{
                 console.log(error)
